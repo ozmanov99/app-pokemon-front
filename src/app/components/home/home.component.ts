c2a116f817a1fedec0e8b7b0859433473b662cc4
ozmanov99 <alik.oz@hotmail.com>
2026-01-30 21:50:55 +0100

    Page visiteur : se connecter ok manque la partie s'inscrire

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  standalone: false,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: false
 })
-export class HomeComponent { }
-
-
+export class HomeComponent {
+  constructor(public authService: AuthService) {}
+}
